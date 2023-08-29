f429275c1964a3efbb8945286f61b35a428d2c6d
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-08-29 15:51:39 +0200

    split into functions for better readability

--- MovieDBApplication/src/main/java/org/CodeForPizza/MovieDBApplication.java
+++ MovieDBApplication/src/main/java/org/CodeForPizza/MovieDBApplication.java
@@ -8,6 +8,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 @Slf4j
@@ -21,28 +22,36 @@ public class MovieDBApplication {
             int year = scanner.nextInt();
 
             // Construct JSON input string
-            String jsonInputString = "{\"title\": \"" + title + "\", \"year\": \"" + year + "\"}";
+            String jsonInputString = constructJsonString(title, year);
 
-            // Send POST request to API-Producer and set content type to JSON
-            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-                HttpPost httpPost = new HttpPost("http://localhost:8080/api/v1/movie/save");
-                httpPost.setHeader("Content-Type", "application/json; utf-8");
-
-                // set the JSON input string as the payload
-                StringEntity stringEntity = new StringEntity(jsonInputString);
-                httpPost.setEntity(stringEntity);
-
-                // execute the POST request
-                try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
-                    int responseCode = response.getStatusLine().getStatusCode();
-                    log.info("Response Code: " + responseCode);
-                } catch (Exception e) {
-                    log.error("Error executing POST request");
-                }
-            }
+            // Send POST request to API-Producer
+            executePostRequest(jsonInputString);
         } catch (Exception e) {
             log.error("Error parsing input");
         }
+    }
+
+    private static void executePostRequest(String jsonInputString) throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost("http://localhost:8080/api/v1/movie/save");
+            httpPost.setHeader("Content-Type", "application/json; utf-8");
+
+            // set the JSON input string as the payload
+            StringEntity stringEntity = new StringEntity(jsonInputString);
+            httpPost.setEntity(stringEntity);
+
+            // execute the POST request
+            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+                int responseCode = response.getStatusLine().getStatusCode();
+                log.info("Response Code: " + responseCode);
+            } catch (Exception e) {
+                log.error("Error executing POST request");
+            }
+        }
+    }
+
+    private static String constructJsonString(String title, int year) {
+        return "{\"title\": \"" + title + "\", \"year\": \"" + year + "\"}";
     }
 
 }

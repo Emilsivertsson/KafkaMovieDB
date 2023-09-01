d2638912154012ae7258f6f5ceb5e1eadd5fbbab
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-09-01 15:00:37 +0200

    Added GUI application

--- GUI/src/main/java/module-info.java
+++ GUI/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.codeforpizza.gui {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires json.simple;
+    requires org.apache.httpcomponents.httpclient;
+    requires org.apache.httpcomponents.httpcore;
 
     opens com.codeforpizza.gui to javafx.fxml;
     exports com.codeforpizza.gui;

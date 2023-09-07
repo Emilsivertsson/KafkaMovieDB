b685e368492df9a116e9269b14cea676cf1e39b4
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-09-07 12:27:35 +0200

    added TopicConfig to database

---
+++ Database-Consumer-producer/src/main/java/org/CodeForPizza/config/TopicConfig.java
@@ -0,0 +1,16 @@
+package org.CodeForPizza.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class TopicConfig {
+
+    @Bean
+    public NewTopic movieTopicJson() {
+        return TopicBuilder.name("returningData")
+                .build();
+    }
+}

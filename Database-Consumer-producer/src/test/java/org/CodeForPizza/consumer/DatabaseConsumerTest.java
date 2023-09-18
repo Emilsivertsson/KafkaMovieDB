6cbde822621a438bb57fb1ff6955712232befdf0
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-09-18 10:24:09 +0200

    fix printout

--- Database-Consumer-producer/src/test/java/org/CodeForPizza/consumer/DatabaseConsumerTest.java
+++ Database-Consumer-producer/src/test/java/org/CodeForPizza/consumer/DatabaseConsumerTest.java
@@ -1,9 +1,6 @@
 package org.CodeForPizza.consumer;
 
-
-import org.CodeForPizza.consumer.DatabaseConsumer.*;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;

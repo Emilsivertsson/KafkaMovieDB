9d5e6770b0baa7e5aafc446ed154d85a6be9d03d
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-09-24 20:27:42 +0200

    moved dependencys

--- Movie/src/main/java/org/CodeForPizza/repository/MovieRepository.java
+++ Movie/src/main/java/org/CodeForPizza/repository/MovieRepository.java
@@ -1,7 +1,5 @@
 package org.CodeForPizza.repository;
 
-
-
 import org.CodeForPizza.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
